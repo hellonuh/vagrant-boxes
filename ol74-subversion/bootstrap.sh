a17cda9c578207a5ec5cfcd11097a69f3ee8e1d3
Neyazul Haque <nuhaque@gmail.com>
2018-03-30 02:35:41 +0530

    Fixed wrong folder path

--- ol74-subversion/bootstrap.sh
+++ ol74-subversion/bootstrap.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/sh
 echo "provisioning the box"
 yum install -y httpd subversion mod_dav_svn
-yum clean all
+yum clean all && rm -rf /var/cache/yum
 mkdir -p /var/local/svn
 mkdir /etc/httpd/dav_svn
 cp /vagrant/files/dav_svn.conf /etc/httpd/config.modules.d/
-cp /vagrant/dav_svn_authz /etc/httpd/dav_svn/
+cp /vagrant/files/dav_svn_authz /etc/httpd/dav_svn/
 cp /vagrant/files/svn-project-creator.sh /usr/local/bin/
-cp /vagrant/files/svn-entrypoint.sh /usr/local/bin/
 chmod a+x /usr/local/bin/svn*
 htpasswd -bc /etc/httpd/dav_svn/dav_svn.passwd admin admin
 htpasswd -bc /etc/httpd/dav_svn/dav_svn.passwd jenkins jenkins
